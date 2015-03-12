87a29048e8a2007c60b6141c94e7df8fd7c77fec
Adrian Mouat <adrian.mouat@gmail.com>
2015-03-12 10:38:27 +0000

    Minor updates.

--- gdim.sh
+++ gdim.sh
@@ -6,20 +6,19 @@
 WORKDIR=/workdir
 
 #Stuff for handling connecting to X session
-#TODO: make these files unique and delete on exit
 XSOCK=/tmp/.X11-unix/
-XAUTH=/tmp/.docker.xauth
-
-#Note that this seems to complain about file not existing the first time
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
-#should create file if it doesn't exist
 
 if [[ $# == 1 ]]; then 
+  XAUTH=$(mktemp -t docker.xauth.XXXXXXXXXX) || { echo "Failed to create temporary file";  exit 1;}
+  xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+  #should create file if it doesn't exist
+
   #get full path
   FILENAME=$(basename $1) 
   ABSFILE=$( cd $(dirname $1); pwd)/$FILENAME
   docker run -v $ABSFILE:$WORKDIR/$FILENAME -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -i -t amouat/gdim:latest $WORKDIR/$FILENAME
+  rm $XAUTH
 else
   echo "$0 expects exactly one file argument"
   exit 1
